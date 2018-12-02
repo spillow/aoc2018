8c03423eca8293d92ce4d74256b6a7f54bdd5d2a
spillow <scott.pillow@gmail.com>
2018-12-01 19:35:12 -0800

    day01b.

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Day01
 
 main :: IO ()
-main = day01a
+main = day01a >> day01b
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,5 +1,6 @@
-module Day01(day01a) where
+module Day01(day01a, day01b) where
 
+import qualified Data.Set as S
 import System.FilePath
 import System.IO
 
@@ -9,4 +10,14 @@ getNumList =
     where path = "data" </> "day01a.input.txt"
 
 day01a :: IO ()
-day01a = getNumList >>= print . sum
+day01a = putStrLn "day01a:" >> getNumList >>= print . sum
+
+day01b :: IO ()
+day01b = do
+    putStrLn "day01b:"
+    nums <- getNumList
+    let freqs = scanl (+) 0 $ cycle nums
+    print $ go S.empty freqs
+    where go _ [] = error "can't find!"
+          go s (x:xs) | S.member x s = x
+          go s (x:xs) = go (S.insert x s) xs
